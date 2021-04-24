b95dcbc5cc68a0f9c072ecaa046abca459d0e8d5
Frank Cao <franktcao@gmail.com>
2021-04-24 18:06:46 -0400

    [WCLT-16] Add docstring

--- src/pipelines/data_science/append_tfidf.py
+++ src/pipelines/data_science/append_tfidf.py
@@ -6,7 +6,24 @@ import numpy as np
 def get_document_frequency(
     doc_count: int, n_posts: int = 1, method: str = "standard", **kwargs,
 ) -> float:
-    """Return document frequency of a term.
+    """
+    Return document frequency of a term.
+
+    :param doc_count:
+        Number of documents (from a corpus) where a given term appears in
+    :param n_posts:
+        Total number of documents in corpus
+    :param method:
+        Method or strategy to calculate the document frequency. Must be one of:
+            * standard
+            * smooth
+            * max
+            * prob
+        See https://en.wikipedia.org/wiki/Tf%E2%80%93idf for more information.
+    :param kwargs:
+        Additional keyword arguments needed to calculate method of document frequency
+    :return:
+        Value of document frequency for the given method
     """
     if method == "standard":
         return doc_count / n_posts
@@ -29,6 +46,18 @@ def get_document_frequency(
 def get_inverse_doc_frequency(
     doc_freq: float, rescale_method: str = "standard"
 ) -> float:
+    """
+    Return inverse document frequency (IDF).
+
+    :param doc_freq:
+        Value of document frequency
+    :param rescale_method:
+        Method used to rescale IDF. Must be one of:
+            * standard
+            * log
+    :return:
+        Value of IDF given the given method
+    """
     inv_doc_freq = 1 / doc_freq
     if rescale_method == "standard":
         return inv_doc_freq
@@ -41,6 +70,24 @@ def get_inverse_doc_frequency(
 def get_term_frequency(
     term_count: int, tot_term_count: int = 1, method: str = "standard", **kwargs,
 ) -> float:
+    """
+    Return the term frequency (TF) for a given method.
+
+    :param term_count:
+        Number of times the term appears in a given document
+    :param tot_term_count:
+        Total number of terms in given document
+    :param method:
+        Method or strategy to used to calculate term frequency. Must be one of:
+            * standard
+            * log_norm
+            * double_k_norm
+        See https://en.wikipedia.org/wiki/Tf%E2%80%93idf for more information.
+    :param kwargs:
+        Additional keyword arguments needed to calculate method of term frequency
+    :return:
+        Value of term frequency for a given method
+    """
     if method == "standard":
         return term_count / float(tot_term_count)
     elif method == "log_norm":
